684bf24a6fc9781241301deca78c1dee39447ca6
Majorbangz88 <joelchimaobichukwu@gmail.com>
2025-02-12 13:41:28 +0100

    first commit

---
+++ src/main/java/com/big_joe/ojemba_bank/OjembaBankApplication.java
@@ -0,0 +1,13 @@
+package com.big_joe.ojemba_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjembaBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OjembaBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/big_joe/ojemba_bank/OjembaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.big_joe.ojemba_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjembaBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
