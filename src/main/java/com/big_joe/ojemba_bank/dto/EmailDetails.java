196e234a457ee8f1efebef04dc91134ff76b3762
Majorbangz88 <joelchimaobichukwu@gmail.com>
2025-03-05 01:56:06 +0100

    created the send email alerts method and added it to the account reg method, also deleted application.properties to fix secret leak

---
+++ src/main/java/com/big_joe/ojemba_bank/dto/EmailDetails.java
@@ -0,0 +1,17 @@
+package com.big_joe.ojemba_bank.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EmailDetails {
+    private String recipient;
+    private String subject;
+    private String messageBody;
+    private String attachments;
+}
