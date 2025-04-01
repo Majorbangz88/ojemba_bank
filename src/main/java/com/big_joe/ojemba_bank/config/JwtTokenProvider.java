48e58d0aaede130f13b1a219b9aceb8556a85991
Majorbangz88 <joelchimaobichukwu@gmail.com>
2025-04-01 11:22:18 +0100

    update: added all necessary endpoints for jwtTokenProvider class

--- src/main/java/com/big_joe/ojemba_bank/config/JwtTokenProvider.java
+++ src/main/java/com/big_joe/ojemba_bank/config/JwtTokenProvider.java
@@ -1,14 +1,68 @@
 package com.big_joe.ojemba_bank.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
 
     @Value("${app.jwt-secret}")
     private String jwtSecret;
+
     @Value("${app.jwt-expiration}")
     private long expiration;
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expiryDate = new Date(currentDate.getTime() + expiration);
+
+        return Jwts.builder()
+                .issuer("Ojemba_Bank")
+                .subject(username)
+                .issuedAt(currentDate)
+                .expiration(expiryDate)
+                .signWith(key(), Jwts.SIG.HS256)
+                .compact();
+    }
+
+    private SecretKey key() {
+        byte[] bytes = Decoders.BASE64.decode(jwtSecret);
+        return Keys.hmacShaKeyFor(bytes);
+    }
+
+    public String getUsername(String token) {
+        Claims claims = Jwts.parser()
+                .verifyWith(key())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(key())
+                    .build()
+                    .parse(token);
+
+            return true;
+        } catch (ExpiredJwtException | IllegalArgumentException | SignatureException | MalformedJwtException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
